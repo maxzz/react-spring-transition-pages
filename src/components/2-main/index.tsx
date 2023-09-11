c539de12e58a1c1ba0170530742f4697052e6b4f
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-11 15:10:17 -0700

    WIP. add initial transition

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -1,10 +1,42 @@
+import { a, useSpringRef, useTransition } from '@react-spring/web';
 import { LoginPage, SigninPage } from '../pages';
+import { useEffect, useState } from 'react';
+
+const Components = [LoginPage, SigninPage];
+const data = [0, 1];
 
 export function SectionMain() {
+    const [index, set] = useState(0);
+    const onClick = () => set(state => (state + 1) % 2);
+
+    const springRef = useSpringRef();
+    const transitions = useTransition(data, {
+        ref: springRef,
+        keys: (item) => item,
+        from: { opacity: 0.5, transform: 'translate3d(20%,0,0)' },
+        to: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+        leave: { opacity: 0.5, transform: 'translate3d(-10%,0,0)' },
+        config: { duration: 2000 },
+    });
+
+    useEffect(() => {
+        springRef.start();
+    }, [index]);
+
     return (
-        <div>
-            <LoginPage />
-            <SigninPage />
+        <div className="flex justify-center h-full" onClick={onClick}>
+
+            {transitions((style, item) => {
+                console.log('style =', style, 'item =', item);
+
+                const Component = Components[item];
+                return (
+                    <a.div style={style}>
+                        <Component />
+                    </a.div>
+                );
+            })}
+
         </div>
     );
 }
