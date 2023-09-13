29e639b5c3948db45d072c62bcc1a28de16bc5e9
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-12 20:01:02 -0700

    WIP. add new test-forms

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -1,47 +1,9 @@
-import { a, useSpringRef, useTransition } from '@react-spring/web';
-import { LoginPage, SigninPage } from '../pages';
-import { useEffect, useState } from 'react';
-
-const Components = [LoginPage, SigninPage];
-//const data = [0, 1];
+import { TestForms } from './1-test-froms';
 
 export function SectionMain() {
-    const [index, set] = useState(0);
-    const onClick = () => set(state => (state + 1) % 2);
-
-    //const springRef = useSpringRef();
-    const [transitions, api] = useTransition(index, () => ({
-        //ref: springRef,
-        //keys: (item) => item,
-        keys: null,
-        from: { opacity: 0.3, transform: 'translate3d(100%,0,0)' },
-        to: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-        leave: { opacity: 0.3, transform: 'translate3d(-50%,0,0)' },
-        config: { duration: 1000 },
-    }));
-
-    useEffect(() => {
-        //springRef.start();
-        api.start({
-            opacity: 1,
-            transform: 'translate3d(20%,0,0)',
-        });
-    }, [index]);
-
     return (
-        <div className="flex justify-center h-full" onClick={onClick}>
-
-            {transitions((style, item) => {
-                console.log('style =', style, 'item =', item);
-
-                const Component = Components[item];
-                return (
-                    <a.div style={style} className="border-red-500 border">
-                        <Component />
-                    </a.div>
-                );
-            })}
-
+        <div className="h-full">
+            <TestForms />
         </div>
     );
 }
