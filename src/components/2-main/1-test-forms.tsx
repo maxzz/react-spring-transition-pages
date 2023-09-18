7c2335ddd34ef892a09fcba879573bbd948641be
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-17 17:54:21 -0700

    WIP. update easing for test-forms

--- src/components/2-main/1-test-forms.tsx
+++ src/components/2-main/1-test-forms.tsx
@@ -15,8 +15,8 @@ export function TestForms() {
         keys: null,
         from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
         enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-        leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', config: { easing: easings.easeInExpo } },
-        config: { duration: 300 },
+        leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', config: { duration: 0 } },
+        config: { easing: easings.easeOutElastic },
     });
 
     useEffect(() => {
@@ -24,14 +24,14 @@ export function TestForms() {
     }, [index]);
 
     return (
-        <div className="flex justify-center overflow-hidden" onClick={onClick}>
+        <div className="relative h-56 flex justify-center overflow-hidden" onClick={onClick}>
 
             {transitions((style, item) => {
                 console.log('style =', style, 'item =', item);
 
                 const Component = Components[item];
                 return (
-                    <a.div style={style} className="border-red-500 border">
+                    <a.div style={style} className="absolute w-full flex items-center justify-center">
                         <Component />
                     </a.div>
                 );
